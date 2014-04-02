1625d49c3b0d9333bfc3ae74823b1ca5218285fe
Alfred Bratterud <alfred.bratterud@hioa.no>
2014-04-02 10:42:47 +0200

    Litt utvidet om virtuelle destruktorer

--- f8_templates/virtual_destructor.cpp
+++ f8_templates/virtual_destructor.cpp
@@ -3,25 +3,39 @@
 using namespace std;
 
 class has_wings{
+  int* i;
 public:
   virtual void flap()=0;
   //virtual ~has_wings()=0; //Not possible!
-  virtual ~has_wings(){};
+  has_wings():i(new int(5)){}
+  ~has_wings(){
+    cout << "Has wings deleted" << endl;
+    delete i;
+  };
 };
 
 
 class horse : public has_wings{
+  int* i;
 public: 
+  horse():i(new int(7)){};
   void flap() override{
     cout << "Horse flapping wings!" << endl;
   }
   
-  ~horse(){};
+  ~horse(){
+    delete i;
+    cout << "Horse deleted" << endl;
+  };
 };
 
 
 int main(){
   has_wings* winged_creature=new horse();
   winged_creature->flap();
-  
+
+  //  horse* pony=new horse();
+
+  delete winged_creature;
+  //delete pony;
 }
